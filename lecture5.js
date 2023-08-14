d5256510e4f9b65518d07bc52c42f3eaf2842517
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-15 02:20:37 +0500

    update

--- lecture5.js
+++ lecture5.js
@@ -3,3 +3,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const data_1 = require("./data");
 console.log(`My Name is ${data_1.data.name}, my age is ${data_1.data.age} my hobies are ${data_1.data.hobbies} I am from ${data_1.data.address.country}, and my city is ${data_1.data.address.city}, and my street address is: ${data_1.data.address.street}.`);
+// syntax error
+let message = "hello world  from Kamran";
+let message1 = "Hello World"; //syntax error
+// typping error message
+// consolle.log();
+console.loger(message);
--- lecture5.ts
+++ lecture5.ts
@@ -4,3 +4,23 @@ import {data} from "./data"
 
 
 console.log (`My Name is ${data.name}, my age is ${data.age} my hobies are ${data.hobbies} I am from ${data.address.country}, and my city is ${data.address.city}, and my street address is: ${data.address.street}.`)
+
+
+// syntax error
+
+
+
+let message = "hello world  from Kamran"
+let message1 = "Hello World";//syntax error
+
+// typping error message
+// consolle.log();
+console.log(message);
+
+// Assinability error
+
+let message2: number
+// message2 = "2" // assignment error
+
+let Rolenumber: number
+Rolenumber  = 1 
